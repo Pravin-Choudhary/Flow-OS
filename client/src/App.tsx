3905f15a985ac5300bf804fe21a7b35da92e3b74
Pravin-Choudhary <pravinchoudhary.aesxa29@gmail.com>
2026-05-21 13:47:30 +0530

    refactor: update client entry points and global styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './components/login-screen/LoginPage';
-import DashboardPage from './components/dasboard/dashboard-page';
+import AppRoutes from "./routes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Login Route */}
-        <Route path="/" element={<LoginPage />} />
-
-        {/* Private/Protected Dashboard Route */}
-        <Route path="/dashboard" element={<DashboardPage />} />
-
-        {/* Fallback to Login */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './globals.css';
-import { ThemeProvider } from './components/theme-provider';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { NuqsAdapter } from "nuqs/adapters/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="flow-os-theme">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+import "./index.css";
+import App from "./App.tsx";
+import QueryProvider from "./context/query-provider.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryProvider>
+      <NuqsAdapter>
+        <App />
+      </NuqsAdapter>
+      <Toaster />
+    </QueryProvider>
+  </StrictMode>
 );
